5c36c746275ffec1d77e825d51d66ee9ee8a1a82
billy <billy@DESKTOP-SPEAK20>
2021-01-03 18:24:12 -0800

    Initial checkin

---
+++ ExpenseTracker/ExpenseTracker/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using ExpenseTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace ExpenseTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            ViewBag.Message = "Your test.";
+
+            var lines = System.IO.File.ReadAllLines(@"C:\Users\billy\Desktop\New folder\DataBase.txt");
+            Expense e = new Expense();
+            foreach (string line in lines)
+            {
+                string[] items = line.Split(' ');
+
+
+                
+                e.Name = items[0];
+                e.Value = Convert.ToDouble(items[1]);
+                break;
+            }
+
+
+
+            //DueDateInformation d = new DueDateInformation();
+            //d.DueDate = new DateTime(2021, 2, 3);
+            //d.Penalty = 35;
+
+            //e.DueDateInfo = d;
+
+            return View(e);
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/Models/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public DueDateInformation DueDateInfo { get; set; }
+    }
+
+    public class DueDateInformation
+    {
+        public DateTime DueDate { get; set; }
+        public double Penalty { get; set; }
+    }
+}
